c992f83bcc39bac654a45d2175bb71b890287940
Sphericalhipster <71931782+Sphericalhipster@users.noreply.github.com>
2020-11-04 22:55:00 -0500

    Add files via upload

---
+++ NARROW-COMPASS/src/main/java/com/thenarrowcompasscompany/narrow/compass/Ciudadela_universitaria.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+package com.thenarrowcompasscompany.narrow.compass;
+import java.util.Scanner;
+/**
+ * Clase encargada de visualizar, y gestionar el acceso a los demás usuarios. 
+ * 
+ * ---Atributos---
+ * 
+ * + id_ciudadela: Int
+ * + nombre_c: String
+ * + direccion_c: String 
+ * + contacto_tel: int 
+ * + contacto_correo: String
+ * 
+ * 
+ */
+public class Ciudadela_universitaria {
+    
+    private void loguin(){
+    
+    
+    }
+    private void logout(){
+    
+    
+    }
+    public void visualizar_info_contactos(){
+    
+    
+    }
+    
+}
---
+++ NARROW-COMPASS/src/main/java/com/thenarrowcompasscompany/narrow/compass/Facultad.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.thenarrowcompasscompany.narrow.compass;
+
+/**
+ * Clase encargada de catalogar las facultades de la universidad nacional. 
+ * 
+ * ---Atributos---
+ * 
+ * + id_facultad: int
+ * + nombre_f: String
+ * + contacto_tel: int
+ * + contacto_correo: String
+ * 
+ * 
+ */
+public class Facultad {
+    
+    public void estado(){
+    
+    
+    }
+    public void ingreso_rol(){
+    
+    
+    }
+    public void visualizar_info(){
+    
+    
+    }
+    
+    
+    
+}
