cbe245ed0d66af8d2bd5455f2971e88bd66a04c1
Sphericalhipster <71931782+Sphericalhipster@users.noreply.github.com>
2020-11-24 15:45:46 -0500

    Add files via upload

---
+++ NARROW-COMPASSV4/src/Controlador/GUINC.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador;
+
+/**
+ *
+ * @author dinan
+ */
+public abstract class GUINC extends javax.swing.JFrame /*implements GUINC1*/ {
+    
+    public abstract void Salir(java.awt.event.ActionEvent evt);
+    public String Mostrar_mensaje(){
+        return "Hasta Luego...";
+    }
+
+}
