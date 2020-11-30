1f6acb25c5047cd97cfab607caccd07ca108978a
Sphericalhipster <71931782+Sphericalhipster@users.noreply.github.com>
2020-11-30 13:27:20 -0500

    Add files via upload

---
+++ NARROW-COMPASS/src/main/java/com/thenarrowcompasscompany/narrow/compass/Estudiante.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.thenarrowcompasscompany.narrow.compass;
+
+/**
+ * Clase que solo permitirá la consulta y visualización de la aplicación a los usuarios
+ * con el rol estudiante sin permisos de cambiar la información suministrada salvo las 
+ * notas rápidas y agenda. 
+ * 
+ * 
+ * ---Atributos---
+ * 
+ * + id_alumn: int
+ * + nombre_alumn: String 
+ * + tel_alumn: int 
+ * + asignaturas_a_ver: String 
+ * + id_horario: String
+ * - rol_estudiante: boolean
+ * 
+ * 
+ */
+public class Estudiante {
+    
+    public void visualizar(){
+    
+    
+    }    
+    public void acceso_notas(){
+    
+    
+    }
+    public void consultar(){
+    
+    
+    }
+}
